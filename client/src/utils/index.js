0f668236de14f04071d5518e225abb7e8df59a17
daviduche03 <daviduche4809@gmail.com>
2023-08-06 02:06:23 +0100

    updated

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -11,6 +11,16 @@ export function getRandomPrompt(prompt) {
 }
 
 export async function downloadImage(_id, photo) {
-  FileSaver.saveAs(photo, `download-${_id}.jpg`);
+  try {
+    // Fetch the image data as a Blob
+    const response = await fetch(photo);
+    const imageBlob = await response.blob();
+
+    // Save the Blob as a file using FileSaver
+    FileSaver.saveAs(imageBlob, `download-${_id}.jpg`);
+  } catch (error) {
+    console.error('Error downloading and saving the image:', error);
+  }
+  
 }
 
